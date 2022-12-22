2eb38fa2a2c2e02d771d8bbcc9ce9f75cc513363
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-12-22 15:36:52 +0100

    add tailed and winged animals who extend Animal

---
+++ src/animals/domain/AnimalWithTail.java
@@ -0,0 +1,21 @@
+package animals.domain;
+
+import java.time.LocalDate;
+
+public abstract class AnimalWithTail extends Animal{
+
+    private double tailLenght;
+
+    protected AnimalWithTail(String name, String favoriteFood, int age, LocalDate joinDate, double weight, double height, double tailLenght) {
+        super(name, favoriteFood, age, joinDate, weight, height);
+        this.tailLenght = tailLenght;
+    }
+
+    public double getTailLenght() {
+        return tailLenght;
+    }
+
+    public void setTailLenght(double tailLenght) {
+        this.tailLenght = tailLenght;
+    }
+}
