db57aa3978cb68246a2ee77e4fc67d10f92afcf3
mauronavi <mauro.naviglio@gmail.com>
2023-01-03 11:24:44 +0100

    add Room attributes, tested on application

--- src/pawtropolis/map/domain/Room.java
+++ src/pawtropolis/map/domain/Room.java
@@ -1,6 +1,55 @@
 package pawtropolis.map.domain;
 
+import pawtropolis.animals.domain.Animal;
+import pawtropolis.game.domain.Item;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Room {
+    private String name;
+    private Collection<Item> items;
+    private Collection<Animal> animals;
+    private Map<String, Room> adiacentRooms;
+
+    public Room(String name){
+        this.name=name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setItems(Collection<Item> items) {
+        this.items = items;
+    }
+
+    public void setAnimals(Collection<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void setAdiacentRooms(Map<String, Room> adiacentRooms) {
+        this.adiacentRooms = adiacentRooms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Collection<Item> getItems() {
+        return items;
+    }
+
+    public Collection<Animal> getAnimals() {
+        return animals;
+    }
+
+    public Map<String, Room> getAdiacentRooms() {
+        return adiacentRooms;
+    }
+
     /*
      * Classe per rappresentare una stanza del gioco
      *
