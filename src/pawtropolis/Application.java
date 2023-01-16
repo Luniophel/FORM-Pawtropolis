2ec587f333446f7a954c841c8e4820cffca04771
Luniophel <stefano.emanuelelucarello@gmail.com>
2023-01-16 16:45:13 +0100

    ref(Application):refactorize GameController instantiation to match properly with his constructor

--- src/pawtropolis/Application.java
+++ src/pawtropolis/Application.java
@@ -1,16 +1,13 @@
 package pawtropolis;
 
-import pawtropolis.game.domain.Item;
-import java.util.ArrayList;
-import java.util.Collection;
+import pawtropolis.game.GameController;
+import pawtropolis.game.domain.Player;
 
 public class Application {
 
     public static void main(String[] args) {
 
-
-        //GameController gc = new GameController(room01, new Player("player01"));
-        //gc.runGame();
-
+        GameController gc = new GameController(new Player("player01"));
+        gc.runGame();
      }
 }
