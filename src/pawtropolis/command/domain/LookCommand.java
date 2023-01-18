3e77364b8e40c5ea277335226e20ebeb71662273
Luniophel <stefano.emanuelelucarello@gmail.com>
2023-01-18 18:14:35 +0100

    ref(LookCommand): change name from Look to LookCommand

--- src/pawtropolis/command/domain/LookCommand.java
+++ src/pawtropolis/command/domain/LookCommand.java
@@ -4,7 +4,7 @@ import pawtropolis.game.GameController;
 
 import java.util.List;
 
-public class Look extends Command{
+public class LookCommand extends Command{
     public void execute(List<String> tokens){
         GameController.getIstance().getCurrentRoom().showInfo();
     }
