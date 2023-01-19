a9bc365be88543711602cdf2bd282a6195cb05cb
Luniophel <stefano.emanuelelucarello@gmail.com>
2023-01-19 16:06:52 +0100

    ref(ExitCommand): change extends Command to implements Command

--- src/pawtropolis/command/domain/ExitCommand.java
+++ src/pawtropolis/command/domain/ExitCommand.java
@@ -4,7 +4,7 @@ import pawtropolis.game.GameController;
 
 import java.util.List;
 
-public class ExitCommand extends Command{
+public class ExitCommand implements Command{
     @Override
     public void execute(List<String> tokens) {
         System.out.println(" GAME OVER.");
