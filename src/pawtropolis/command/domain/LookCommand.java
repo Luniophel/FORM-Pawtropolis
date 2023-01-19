cc20fd70be46f79a2531fea4c9b5f3f070aa5e22
Luniophel <stefano.emanuelelucarello@gmail.com>
2023-01-19 16:07:39 +0100

    ref(LookCommand): change extends Command to implements Command

--- src/pawtropolis/command/domain/LookCommand.java
+++ src/pawtropolis/command/domain/LookCommand.java
@@ -1,11 +1,10 @@
 package pawtropolis.command.domain;
 
 import pawtropolis.game.GameController;
-import pawtropolis.map.MapController;
 
 import java.util.List;
 
-public class LookCommand extends Command{
+public class LookCommand implements Command{
     public void execute(List<String> tokens){
 
         GameController.getIstance().showRoomInfo();
