eefb02b281537217fca29ee38f2dffc0cfd217d1
Luniophel <stefano.emanuelelucarello@gmail.com>
2023-01-19 16:08:00 +0100

    ref(UnknownCommand): change extends Command to implements Command

--- src/pawtropolis/command/domain/UnknownCommand.java
+++ src/pawtropolis/command/domain/UnknownCommand.java
@@ -2,7 +2,7 @@ package pawtropolis.command.domain;
 
 import java.util.List;
 
-public class UnknownCommand extends Command{
+public class UnknownCommand implements Command{
     @Override
     public void execute(List<String> tokens) {
         System.out.println("Unknown command !");
